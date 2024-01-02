144647515679f760856b40a334ab1cf30697e7db
Yulia  Demir <Nightelfin91@gmail.com>
2024-01-02 20:18:38 +0300

    ReadMe file was added. Main class was changed. The program has been completed and formalized.

---
+++ untitled6/ReadMe.md
@@ -0,0 +1,2 @@
+This program finds the shortest and longest number of the entered numbers.
+The count of numbers is determined from the console, but must be less than 10.
--- untitled6/src/main/java/org/example/Main.java
+++ untitled6/src/main/java/org/example/Main.java
@@ -1,7 +1,48 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Class {@code Main} is the main class of the program.
+ * The program starts from method main of {@code Main} class.
+ *
+ * @author Yulia Demir
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(55);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a count of numbers less than 10: ");
+        try {
+            int countOfNumbers = scanner.nextInt();
+            if (countOfNumbers >= 10)
+                throw new IllegalArgumentException("");
+            scanner.nextLine();
+
+            String[] numbers = new String[countOfNumbers];
+            String number;
+            String shortNumber = null, longNumber = null;
+            for (int i = 0; i < countOfNumbers; i++) {
+                number = scanner.nextLine();
+                try {
+                    Double.parseDouble(number.trim());
+                }
+                catch (Exception e) {
+                    System.err.println("It is not number!");
+                    System.exit(1);
+                }
+                numbers[i] = number;
+                shortNumber = shortNumber == null ? number :
+                        shortNumber.length() > number.length() ? number : shortNumber;
+                longNumber = longNumber == null ? number :
+                        longNumber.length() < number.length() ? number : longNumber;
+            }
+            System.out.println("Numbers: " + Arrays.toString(numbers));
+            System.out.println("The shortest number is " + shortNumber);
+            System.out.println("The longest number is " + longNumber);
+        }
+        catch (Exception e) {
+            System.out.println("The incorrect count of numbers were entered!");
+        }
     }
 }
